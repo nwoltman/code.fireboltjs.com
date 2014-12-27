ca8fd8302a776b3537bee78bb4fcb20360bea14a
Nathan Woltman <nwoltman@outlook.com>
2014-12-27 03:14:59 -0500

    Improved automatic deployment to S3
    Now only the necessary files are uploaded to S3 instead of the entire project.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Extract the version number from the latest minified file
+  var code = require('fs').readFileSync('firebolt.min.js', {encoding: 'utf8'});
+  var version = /v(\d+\.\d+\.\d+)\b/.exec(code)[1];
+
+  // Project configuration
+  grunt.initConfig({
+    copy: {
+      main: {
+        nonnull: true,
+        expand: true,
+        src: ['*.html', 'firebolt*', 'Firebolt.zip', 'v/' + version + '/*'],
+        dest: 'dist/'
+      }
+    }
+  });
+
+  // Load the copy task
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Register the grunt task
+  grunt.registerTask('default', ['copy']);
+};
